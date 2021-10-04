f8eb4096396ee1d4ae39a7d4e921092dfe808d65
ArnoldFloss <91864401+ArnoldFloss@users.noreply.github.com>
2021-10-04 10:18:13 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	var app = fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error { return c.SendString("Hello World 👋") })
+	app.Listen(":8080")
+}
